281dad464f7ff47cc58a00eb6a2e1494c6d366fd
danisluis7 <lorence@enclave.vn>
2018-07-24 07:33:46 +0700

    Working with ajax

---
+++ public/js/incense.js
@@ -0,0 +1,58 @@
+/**
+ * Created by vuongluis on 7/22/2018.
+ */
+$(document).ready(function () {
+    $(function () {
+        // Replace the <textarea id="editor1"> with a CKEditor
+        // instance, using default configuration.
+        CKEDITOR.replace('incenseCreate')
+        CKEDITOR.replace('incenseDetailEdit')
+        CKEDITOR.replace('editor1')
+        CKEDITOR.replace('editor2')
+        //bootstrap WYSIHTML5 - text editor
+        $('.textarea').wysihtml5()
+    });
+
+    $('#incenseTable').dataTable({
+        "pageLength": 18,
+        "lengthMenu": [[5,10,15,-1], [5,10,15,'All']],
+        'paging'      : true,
+        'lengthChange': true,
+        'searching'   : true,
+        'ordering'    : true,
+        'info'        : true,
+        'autoWidth'   : false,
+        "processing": true,
+        "serverSide": true,
+
+        "ajax": {
+            url: '/quan-tri/nuoc-hoa/nhom-huong/loading_incense',
+            type: "GET"
+        },
+
+        "columns": [
+            { "data": "id" },
+            { "data": "name" },
+            { "data": "description" },
+            { "data": "detail" },
+            { "data": "link" },
+            { "data": "manipulation", "render": function (id) {
+                return '<div class="text-center"><a onclick= "showEditIncense('+id+')"><img src="/img/icon-control/icon_edit.svg"  width="24px" height="24px" alt="Update Icon"></a>'
+                +'<span>  </span>'+'<a href="/quan-tri/nuoc-hoa/nhom-huong/xoa/'+id+'" onclick="deleteIncenseFunction('+id+')"><img src="/img/icon-control/icon_delete.svg"  width="24px" height="24px" alt="Update Icon"></a></div>';
+            }}
+        ],
+
+        "language": {
+            "lengthMenu": "Hiển thị các bản ghi _MENU_ trên mỗi trang",
+            "zeroRecords": "Không tìm thấy - xin lỗi",
+            "info": "Hiển thị _START_ đến _END_ trong số _TOTAL_ mục",
+            "infoEmpty": "Không có bản ghi nào",
+            "search": "Tìm kiếm: ",
+            "paginate": {
+                "previous": "Trước",
+                "next": "Sau"
+            },
+            "infoFiltered": "(được lọc từ tổng số bản ghi _MAX_)"
+        }
+    });
+});
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -124,17 +124,9 @@
 <script src="{{ URL::asset('plugins/input-mask/jquery.inputmask.js') }}"></script>
 <script src="{{ URL::asset('plugins/input-mask/jquery.inputmask.date.extensions.js') }}"></script>
 <script src="{{ URL::asset('plugins/input-mask/jquery.inputmask.extensions.js') }}"></script>
+<script src="{{ URL::asset('js/incense.js') }}"></script>
 <script>
-    $(function () {
-        // Replace the <textarea id="editor1"> with a CKEditor
-        // instance, using default configuration.
-        CKEDITOR.replace('incenseCreate')
-        CKEDITOR.replace('incenseDetailEdit')
-        CKEDITOR.replace('editor1')
-        CKEDITOR.replace('editor2')
-        //bootstrap WYSIHTML5 - text editor
-        $('.textarea').wysihtml5()
-    })
+
 
     $(function () {
         // Replace the <textarea id="editor1"> with a CKEditor
@@ -145,7 +137,7 @@
         CKEDITOR.replace('editor2')
         //bootstrap WYSIHTML5 - text editor
         $('.textarea').wysihtml5()
-    })
+    });
 
     $(function () {
         //Initialize Select2 Elements
@@ -154,53 +146,9 @@
         $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
         //Money Euro
         $('[data-mask]').inputmask()
-    })
+    });
 
     $(document).ready(function(){
-        $('#incenseTable').dataTable({
-            "pageLength": 18,
-            "lengthMenu": [[5,10,15,-1], [5,10,15,'All']],
-            'paging'      : true,
-            'lengthChange': true,
-            'searching'   : true,
-            'ordering'    : true,
-            'info'        : true,
-            'autoWidth'   : false,
-            "processing": true,
-            "serverSide": true,
-
-            "ajax": {
-                url: "{!! route('admin.perfume.incense.index.incenseDataTables') !!}",
-                type: "GET"
-            },
-
-            "columns": [
-                { "data": "id" },
-                { "data": "name" },
-                { "data": "description" },
-                { "data": "detail" },
-                { "data": "link" },
-                { "data": "manipulation", "render": function ( id) {
-                    return '<div class="text-center"><a onclick= "showEditIncense('+id+')"><img src="{{URL::asset('img/icon-control/icon_edit.svg')}}"  width="24px" height="24px" alt="Update Icon"></a>'
-                        +'<span>  </span>'+'<a href="/quan-tri/nuoc-hoa/nhom-huong/xoa/'+id+'" onclick="deleteIncenseFunction('+id+')"><img src="{{URL::asset('img/icon-control/icon_delete.svg')}}"  width="24px" height="24px" alt="Update Icon"></a></div>';
-                }}
-            ],
-
-            "language": {
-                "lengthMenu": "Hiển thị các bản ghi _MENU_ trên mỗi trang",
-                "zeroRecords": "Không tìm thấy - xin lỗi",
-                "info": "Hiển thị _START_ đến _END_ trong số _TOTAL_ mục",
-                "infoEmpty": "Không có bản ghi nào",
-                "search": "Tìm kiếm: ",
-                "paginate": {
-                    "previous": "Trước",
-                    "next": "Sau"
-                },
-                "infoFiltered": "(được lọc từ tổng số bản ghi _MAX_)"
-            }
-
-        });
-
         $('#styleTable').dataTable({
             "pageLength": 18,
             "lengthMenu": [[5,10,15,-1], [5,10,15,'All']],
@@ -224,7 +172,7 @@
                 { "data": "description" },
                 { "data": "detail" },
                 { "data": "link" },
-                { "data": "manipulation", "render": function ( id) {
+                { "data": "manipulation", "render": function (id) {
                     return '<div class="text-center"><a onclick= "showEditStyle('+id+')"><img src="{{URL::asset('img/icon-control/icon_edit.svg')}}"  width="24px" height="24px" alt="Update Icon"></a>'
                         +'<span>  </span>'+'<a href="/quan-tri/nuoc-hoa/phong-cach/xoa/'+id+'" onclick="deleteStyleFunction('+id+')"><img src="{{URL::asset('img/icon-control/icon_delete.svg')}}"  width="24px" height="24px" alt="Update Icon"></a></div>';
                 }}
