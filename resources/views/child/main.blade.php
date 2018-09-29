093901c89657b693761a525bf235f2e75ede6260
danisluis7 <lorence@enclave.vn>
2018-09-29 13:17:46 +0700

    Done

--- resources/views/child/main.blade.php
+++ resources/views/child/main.blade.php
@@ -21,15 +21,15 @@
                         <div class="responsiveImgOuter" align="center">
                             <img class="img-responsive" src="{{URL::asset('img/title_perfume_name.png')}}"/>
                         </div>
-                        <p class="text-center ajdustDescription" class="text-center">Chuyên Nước Hoa Pháp Chính Hãng</p>
-                        <p class="exploreInner" align="center"><a id="ajdustBtnExplore" role="button" href="#">Khám phá</a></p>
+                        <p class="text-center ajdustDescription" class="text-center">Nước Hoa giá sỉ - bán buôn nước hoa - chuyên cung cấp phân phối sỉ nước hoa - nguồn hàng nước hoa</p>
+                        <p class="exploreInner" align="center"><a id="ajdustBtnExplore" href="#explorePerfume" class="smooth-goto" role="button">Khám phá</a></p>
                         <div align="center">
                             <p id="ajdustImageReadmore"><img src="{{URL::asset('img/readmore.png')}}"/></p>
                         </div>
                     </div>
                 </div>
                 <div class="item">
-                    <img src="{{URL::asset('img/1.jpg')}}" alt="Slide Image" class="img-fluid tales">
+                    <img src="{{URL::asset('img/4.png')}}" alt="Slide Image" class="img-fluid tales">
                 </div>
                 <div class="item"><img src="{{URL::asset('img/2.jpg')}}" alt="Slide Image" class="img-fluid tales"></div>
                 <div class="item active"><img src="{{URL::asset('img/3.jpg')}}" alt="Slide Image" class="img-fluid tales"></div>
@@ -54,15 +54,37 @@
             </div>
             <p class="text-center ajdustDescription sologan">Nước Hoa Pháp Chính Hãng</p>
             <div class="hotPerfumeOuter">
-                @if(isset($perfumes))
-                    @foreach($perfumes as $perfume)
+                @if(isset($hotPerfumes))
+                    @foreach($hotPerfumes as $perfume)
                         <div class="col-md-3 col-sm-6 col-xs-12 text-center wow fadeInUp">
                             <div class="thumbnail">
-                                <img src="{{URL::asset('perfume/'.$perfume->path_image)}}" alt="perfume" width="240px;" height="240px;"/>
+                                <img src="{{URL::asset('perfume/'.$perfume->path_image)}}" alt="perfume" style="width:240px; height:240px;"/>
                                 <div class="caption">
-                                    <h2 class="text-center">{{ $perfume->name }}</h2>
-                                    <p class="text-center description">{{ strlen(strip_tags($perfume->description)) > 200 ? substr(strip_tags($perfume->description),0,200)."..." : strip_tags($perfume->description)}}</p>
-                                    <p class="text-center currency">{{ $perfume->original_price }}</p>
+                                    <h2 class="text-center">
+                                        <?php
+                                        if (strlen(strip_tags($perfume->name)) > 21) {
+                                            echo substr(strip_tags($perfume->name),0,21)."...";
+                                        } else {
+                                            echo $perfume->name;
+                                        }
+                                        ?>
+                                    </h2>
+                                    <p class="text-center description">
+                                        <?php
+                                        if (strlen(strip_tags($perfume->description)) > 120) {
+                                            echo substr(strip_tags($perfume->description),0,120)."...";
+                                        } else {
+                                            if (strlen(strip_tags($perfume->description)) < 40)
+                                                echo strip_tags($perfume->description)."</br></br>";
+                                            else
+                                                echo strip_tags($perfume->description)."</br>";
+                                        }
+                                        ?>
+                                    </p>
+                                    <p class="text-center currency">
+                                    <?php
+                                    echo number_format($perfume->original_price, 0, '', ','). " VND";
+                                    ?>
                                 </div>
                             </div>
                         </div>
@@ -70,7 +92,7 @@
                 @endif
             </div>
             <div style="margin: 0 auto;">
-                <p class="text-center btnDetail"><span>Xem chi tiết</span></p>
+                <p class="text-center btnDetail"><span><a id="perfume-list-host" href="javascript:void(0)"><span>Xem chi tiết</span></a></span></p>
             </div>
         </div>
     </div>
@@ -85,61 +107,26 @@
             </div>
             <p class="text-center ajdustDescription sologan">Nước Hoa Pháp Chính Hãng</p>
             <div class="newPerfumeOuter">
-                <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp">
-                    <div class="thumbnail overrideImage">
-                        <img src="{{URL::asset('img/perfume_demo_2.png')}}"/>
-                        <div class="textPerfume">
-                            <p class="namePerfume">Nos Parfums</p>
+                @if(isset($newPerfumes))
+                    @foreach($newPerfumes as $perfume)
+                        <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp">
+                            <div class="thumbnail overrideImage">
+                                <img src="{{URL::asset('perfume/'.$perfume->path_image)}}" style="width: 362px; height: 521px;" />
+                                <div class="textPerfume fdw-background" style="opacity: 0;">
+                                    <p class="namePerfume">Nos Parfums</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp">
-                    <div class="thumbnail overrideImage">
-                        <img src="{{URL::asset('img/perfume_demo_2.png')}}"/>
-                        <div class="textPerfume">
-                            <p class="namePerfume">Nos Parfums</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp">
-                    <div class="thumbnail overrideImage">
-                        <img src="{{URL::asset('img/perfume_demo_2.png')}}"/>
-                        <div class="textPerfume">
-                            <p class="namePerfume">Nos Parfums</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="newPerfumeOuter">
-                <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp">
-                    <div class="thumbnail overrideImage">
-                        <img src="{{URL::asset('img/perfume_demo_2.png')}}"/>
-                        <div class="textPerfume">
-                            <p class="namePerfume">Nos Parfums</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp">
-                    <div class="thumbnail overrideImage">
-                        <img src="{{URL::asset('img/perfume_demo_2.png')}}"/>
-                        <div class="textPerfume">
-                            <p class="namePerfume">Nos Parfums</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp">
-                    <div class="thumbnail overrideImage">
-                        <img src="{{URL::asset('img/perfume_demo_2.png')}}"/>
-                        <div class="textPerfume">
-                            <p class="namePerfume">Nos Parfums</p>
-                        </div>
-                    </div>
+                    @endforeach
+                @endif
+                <div style="margin: 20px auto;">
+                    <p class="text-center btnDetail"><span>Xem chi tiết</span></p>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="consultOuter">
+    <div class="consultOuter" id="explorePerfume">
         <div class="jumbotron titleExploreInner">
             <div>
                 <div align="center">
@@ -156,7 +143,7 @@
                     Nước hoa đã trở thành của riêng mình khi Louis XV lên ngôi trong thế kỷ 18.<br> Việc sử dụng nước hoa ở Pháp tăng đều đặn. Vào thế kỷ 18, cây thơm<br> đã được trồng ở vùng Grasse của Pháp để cung cấp cho ngành công nghiệp<br> nước hoa đang phát triển với nguyên liệu thô.
                 </div>
                 <div>
-                    <p class="btnExplore"><span>Khám phá</span></p>
+                    <p class="btnExplore"><span><a href="">Khám phá</a></span></p>
                 </div>
             </div>
             <div class="consultPerfumeOuter">
@@ -165,7 +152,7 @@
                         <div class="imgInfo">
                             <img src="{{URL::asset('img/perfume_more_detail.png')}}"/>
                         </div>
-                        <div class="textInfo">
+                        <div class="textInfo" align="center">
                             <h3>NOS POINTS DE VENTE</h3>
                             <hr class="hr">
                             <p>Nước hoa đã trở thành của riêng mình khi Louis XV lên ngôi trong thế kỷ 18</p>
@@ -176,7 +163,7 @@
                         <div class="imgInfo">
                             <img src="{{URL::asset('img/perfume_more_detail.png')}}"/>
                         </div>
-                        <div class="textInfo">
+                        <div class="textInfo" align="center">
                             <h3>NOS POINTS DE VENTE</h3>
                             <hr class="hr">
                             <p>Nước hoa đã trở thành của riêng mình khi Louis XV lên ngôi trong thế kỷ 18</p>
